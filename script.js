7ed9739da21e2ae40dfe64905edfed0a0614f885
sagni <sgnsyn45@gmail.com>
2025-04-11 12:59:01 +0300

    Implemented clock updating function

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hello")
+const SECONDS = 1000
+const timeP = document.querySelector(".time-card .card-img")
+
+function getCurrentTimeFormatted() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds()
+    return [`${hours}:${minutes}`,seconds]
+
+}
+
+function init(){
+    const [time, seconds] = getCurrentTimeFormatted()
+    timeP.textContent = time 
+    return 60-seconds
+}
+
+updateTimer= init()
+
+setTimeout( ()=>{
+    init()
+    setInterval(init, 60*SECONDS)
+} ,updateTimer * SECONDS) 
+
