879fa5daf6516ab9ee447762dbb8038d6fa6e584
sgnsyn <sgnsyn45@gmail.com>
2025-08-14 17:03:54 +0300

    Added the questions to exercies.jso

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-const SECONDS = 1000
-const timeP = document.querySelector(".time-card .card-img")
+const SECONDS = 1000;
+const timeP = document.querySelector(".time-card .card-img");
 
 function getCurrentTimeFormatted() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds()
-    return [`${hours}:${minutes}`,seconds]
-
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds();
+  return [`${hours}:${minutes}`, seconds];
 }
 
-function init(){
-    const [time, seconds] = getCurrentTimeFormatted()
-    timeP.textContent = time 
-    return 60-seconds
+function init() {
+  const [time, seconds] = getCurrentTimeFormatted();
+  timeP.textContent = time;
+  return 60 - seconds;
 }
 
-updateTimer= init()
+updateTimer = init();
 
-setTimeout( ()=>{
-    init()
-    setInterval(init, 60*SECONDS)
-} ,updateTimer * SECONDS) 
-
+setTimeout(() => {
+  init();
+  setInterval(init, 60 * SECONDS);
+}, updateTimer * SECONDS);
